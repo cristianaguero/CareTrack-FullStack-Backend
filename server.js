63d0143e19abf2d6370e2c0d0ee056a186b8c88f
cristian <cristian.s.aguero@icloud.com>
2023-11-08 18:45:48 +0200

    trying cors

--- server.js
+++ server.js
@@ -16,7 +16,7 @@ const allowedOrigins = [process.env.FRONTEND_URL];
 
 const corsOptions = {
     origin: function (origin, callback) {
-        if (allowedOrigins.indexOf(origin) !== -1) {
+        if (allowedOrigins.indexOf(origin) !== -1 || !origin) {
             callback(null, true)
         } else {
             callback(new Error('Not allowed by CORS'));
